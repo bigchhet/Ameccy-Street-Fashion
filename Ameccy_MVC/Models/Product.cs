a2ec203540e184544df200daf5cf0609a7e6d778
Chhai <Chhai@DESKTOP-F7V4U3V>
2021-08-20 10:48:50 -0700

    Add project files.

---
+++ Ameccy_MVC/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ameccy_MVC.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public double Price { get; set; }
+    }
+}
